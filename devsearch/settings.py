2d7f95e42a0d9eb24ebd37ae8e9d3f1b8961fb8d
Akash04Patel <88400162+Akash04Patel@users.noreply.github.com>
2022-09-03 19:32:42 +0530

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--4iws!uc#2j2cwb2llj!)k&lw3=+zy^2cxqrdj2h=nai8eiq*8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
